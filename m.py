9299f44060a85c967cc721a3bb748dbe9a692a0c
Kimyobu <91616822+Kimyobu@users.noreply.github.com>
2023-07-11 19:08:48 +0700

    Update m.py

--- m.py
+++ m.py
@@ -1,36 +1,36 @@
 import os
 import subprocess
 class CRD:
-    def __init__(self, user, c, p, a):
+    def __init__(self, user, a, c, p):
         os.system("apt update")
         self.installCRD()
         self.installDesktopEnvironment()
         self.installGoogleChorme()
-        self.finish(user, c, p, a)
-        print("\nRDP created succesfully move to https://remotedes" + "ktop.google.com/access")
+        self.finish(user, a, c, p)
+        print("\nRDP created succesfully move to https://remotedesktop.google.com/access")
 
     @staticmethod
     def installCRD():
         print("Installing Chrome Remote Desktop")
-        subprocess.run(['wget', 'https://dl.google.com/lin' + 'ux/direct/chrome-' + 'remot' + 'e-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
-        subprocess.run(['dpkg', '--install', 'chro' + 'me-remot' + 'e-deskt' + 'op_current_amd64.deb'], stdout=subprocess.PIPE)
+        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
         subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
 
     @staticmethod
     def installDesktopEnvironment():
         print("Installing Desktop Environment")
         os.system("export DEBIAN_FRONTEND=noninteractive")
-        os.system("apt install --assume-yes xfce4 deskt" + "op-base xfce4-terminal")
-        os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remot"+"e-desktop-session'")
-        os.system("apt remove --assume-"+"yes gnome-terminal")
-        os.system("apt install --assume-y"+"es xscreensaver")
-        os.system("systemctl disable li" + "ghtdm.service")
+        os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
+        os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
+        os.system("apt remove --assume-yes gnome-terminal")
+        os.system("apt install --assume-yes xscreensaver")
+        os.system("systemctl disable lightdm.service")
 
     @staticmethod
     def installGoogleChorme():
         print("Installing Google Chrome")
-        subprocess.run(["wget", "https://dl.googl" + "e.com/linux/direct/google-c" + "hrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
-        subprocess.run(["dpkg", "--install", "google-chrom" + "e-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+        subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+        subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
         subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
 
     @staticmethod
@@ -38,7 +38,7 @@ class CRD:
         print("Finalizing")
         if Autostart:
             os.makedirs(f"/home/{user}/.config/autostart", exist_ok=True)
-            link = "https://colab.research.google.com/github/PradyumnaKrishna/Colab-H" + "acks/blob/master/Colab%20RDP/Colab%20RDP.ipynb"
+            link = "https://colab.research.google.com/github/PradyumnaKrishna/Colab-Hacks/blob/master/Colab%20RDP/Colab%20RDP.ipynb"
             colab_autostart = """[Desktop Entry]
 Type=Application
 Name=Colab
@@ -51,10 +51,10 @@ X-GNOME-Autostart-enabled=true""".format(link)
             os.system(f"chmod +x /home/{user}/.config/autostart/colab.desktop")
             os.system(f"chown {user}:{user} /home/{user}/.config")
 
-        os.system(f"adduser {user} chrome-re" + "mote-desktop")
+        os.system(f"adduser {user} chrome-remote-desktop")
         command = f"{CRP} --pin={Pin}"
         os.system(f"su - {user} -c '{command}'")
-        os.system("service chrome-rem" + "ote-desktop start")
-
+        os.system("service chrome-remote-desktop start")
+        
 
         print("Finished Succesfully")
